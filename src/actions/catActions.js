8871d0b1a24a470c224e5f15794c015f4df0696f
jaredjames2020 <jaredjames2020@gmail.com>
2020-06-28 15:38:57 -0400

    thunk lab complete commit

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,18 @@
 // write and export your action creator function here
+export const fetchCats = () => {
+     const API_ENDPOINT = 'https://learn-co-curriculum.github.io/cat-api/cats.json'
+     return dispatch => {
+          dispatch({ type: 'LOADING_CATS' });
+          fetch(API_ENDPOINT)
+               .then(response => response.json())
+               .then(
+                    cats => {
+                         dispatch({
+                              type: 'ADD_CATS',
+                              cats: cats.images
+                         })
+                    }
+               )
+               .catch(error => console.log(error))
+     }
+}
